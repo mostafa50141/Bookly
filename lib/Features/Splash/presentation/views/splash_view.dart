12d3882dfff38d73fb35809a23d8f6b9052329ef
mostafa50141 <abdelazizmostafa160@gmail.com>
2026-01-27 21:06:54 +0200

    first commit

---
+++ lib/Features/Splash/presentation/views/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Features/Splash/presentation/views/widgets/splash_view_body.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SplashViewBody());
+  }
+}
