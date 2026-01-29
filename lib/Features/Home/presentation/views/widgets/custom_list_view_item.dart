733ad728762cb584a5930c41d1b1bfa31e8ae4b9
mostafa50141 <abdelazizmostafa160@gmail.com>
2026-01-29 21:04:58 +0200

    add custom list view item to home view

---
+++ lib/Features/Home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/Utils/assetsData.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          //width: MediaQuery.of(context).size.width,
+          //height: MediaQuery.of(context).size.height * 0.25,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Features/Home/presentation/views/widgets/custom_appBar.dart';
+import 'package:flutter_application_1/Features/Home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:gap/gap.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [CustomAppBar(), const gap()]);
+    return Column(children: [CustomAppBar(), Gap(2), CustomListViewItem()]);
   }
 }
