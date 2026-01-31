896058830e42a2c8356dc9b17632d650a58a3bf2
mostafa50141 <abdelazizmostafa160@gmail.com>
2026-01-31 19:57:16 +0200

    Best seller item done

---
+++ lib/Features/Home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Features/Home/presentation/views/widgets/book_rating.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/core/Utils/assetsData.dart';
+import 'package:flutter_application_1/core/Utils/styles.dart';
+import 'package:gap/gap.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+
+          const Gap(25),
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    'Harry Potter and the Goblet of Fire',
+                    style: Styles.textStyle20.copyWith(
+                      fontFamily: kGtSectraFile,
+                    ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                const Gap(2),
+
+                Text(
+                  'J.K. Rowling',
+                  style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+                ),
+                const Gap(2),
+
+                Row(
+                  children: [
+                    Text(
+                      '10.99 \$',
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+
+                    const BookRating(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Features/Home/presentation/views/widgets/book_rating.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/Utils/styles.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          FontAwesomeIcons.solidStar,
+          color: const Color(0xffFFDD4F),
+          size: 16,
+        ),
+        const Gap(6),
+
+        Text('4.8', style: Styles.textStyle16),
+        const Gap(5),
+
+        Text(
+          '(2000)',
+          style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+        ),
+      ],
+    );
+  }
+}
