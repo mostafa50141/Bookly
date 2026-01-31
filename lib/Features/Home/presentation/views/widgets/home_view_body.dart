6d63243ef87d7e2c9876ac98b38c17717c8dde12
mostafa50141 <abdelazizmostafa160@gmail.com>
2026-01-31 19:03:23 +0200

    create app styles

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Features/Home/presentation/views/widgets/custom_appBar.dart';
 import 'package:flutter_application_1/Features/Home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/core/Utils/assetsData.dart';
 import 'package:flutter_application_1/core/Utils/styles.dart';
 import 'package:gap/gap.dart';
 
@@ -13,12 +15,73 @@ class HomeViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
+        children: [
           CustomAppBar(),
           Gap(10),
           FeaturedBooksListView(),
           Gap(30),
-          Text('Best Sellers', style: Styles.titleMedium),
+          Text('Best Sellers', style: Styles.textStyle18),
+          Gap(15),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          const Gap(20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Harry Potter', style: Styles.textStyle18),
+              const Gap(6),
+              Text(
+                'J.K. Rowling',
+                style: Styles.textStyle18.copyWith(color: Colors.white70),
+              ),
+              const Gap(20),
+              Row(
+                children: [
+                  Text('10.99 \$', style: Styles.textStyle18),
+                  const Gap(20),
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.star, color: Colors.amber[700], size: 20),
+                  const Gap(5),
+                  Text('4.8', style: Styles.textStyle18),
+                  const Gap(10),
+                  Text(
+                    '(2000)',
+                    style: Styles.textStyle18.copyWith(color: Colors.white70),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
