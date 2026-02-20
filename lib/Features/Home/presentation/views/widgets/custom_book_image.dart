97871342e7d39fa3d01f773f5f55fb69b15d3957
mostafa50141 <abdelazizmostafa160@gmail.com>
2026-02-20 02:09:10 +0200

    display feature books list view part 2

--- lib/Features/Home/presentation/views/widgets/custom_book_image.dart
+++ lib/Features/Home/presentation/views/widgets/custom_book_image.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/core/Utils/assetsData.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({super.key});
+  const CustomBookImage({super.key, required this.imageUrl});
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +10,11 @@ class CustomBookImage extends StatelessWidget {
       child: AspectRatio(
         aspectRatio: 2.6 / 4,
         child: Container(
-          //padding: const EdgeInsets.symmetric(horizontal: 8),
-          //margin: const EdgeInsets.only(right: 16),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
             color: Colors.red,
-            image: const DecorationImage(
-              image: AssetImage(AssetsData.testImage),
+            image: DecorationImage(
+              image: NetworkImage(imageUrl),
               fit: BoxFit.fill,
             ),
           ),
